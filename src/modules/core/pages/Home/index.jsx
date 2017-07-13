b8a07e7b331f0dc531dd294b574dccd0f0925e95
victormier <victormier@gmail.com>
2017-07-13 19:14:51 +0200

    Add padding to main grid

--- src/modules/core/pages/Home/index.jsx
+++ src/modules/core/pages/Home/index.jsx
@@ -5,10 +5,10 @@ import gridStyles from 'styles/base/grid.scss';
 
 
 const Home = () => (
-  <Grid fluid className={gridStyles.fullHeight}>
+  <Grid fluid className={gridStyles.mainGrid}>
     <Row bottom="xs" className={gridStyles.fullHeight}>
       <Col xs={6}><Button text="Log in" /></Col>
-      <Col xs={6}><Button text="Sign up!" /></Col>
+      <Col xs={6}><Button text="Sign up" /></Col>
     </Row>
   </Grid>
 );

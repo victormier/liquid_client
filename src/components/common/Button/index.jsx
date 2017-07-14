8b8a47786214f850589b8e4e18c9eab284c3b635
victormier <victormier@gmail.com>
2017-07-14 13:38:25 +0200

    Add colors to buttons

--- src/components/common/Button/index.jsx
+++ src/components/common/Button/index.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
-const Button = props => (
-  <button className={styles.button}>{ props.text }</button>
-);
+const Button = props => <button className={styles[props.color || 'default']}>{ props.text }</button>;
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  color: PropTypes.oneOf(['blue', 'transparent']),
 };
 
 

efb23c7f7ac1b9d35f286a8c1fa7472c589e9b1c
victormier <victormier@gmail.com>
2017-08-23 12:59:45 +0200

    Add middleware to apollo for inserting auth header

--- src/config/apolloClient.js
+++ src/config/apolloClient.js
@@ -4,11 +4,24 @@ import ApolloClient, {
 } from 'apollo-client';
 import { API_URL } from './index';
 
+const networkInterface = createNetworkInterface({
+  uri: `${API_URL}/graphql`,
+});
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {};  // Create the header object if needed.
+    }
+    // get the authentication token from local storage if it exists
+    const token = window.localStorage.getItem('auth_token');
+    req.options.headers.authorization = token ? `Bearer ${token}` : null;
+    next();
+  },
+}]);
 
 const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: `${API_URL}/graphql`,
-  }),
+  networkInterface,
   queryTransformer: addTypeName,
   dataIdFromObject: o => o.id,
 });

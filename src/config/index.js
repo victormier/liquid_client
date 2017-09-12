2f68049dd993dfe57db657f961f67617b74a5fe2
victormier <victormier@gmail.com>
2017-09-12 19:00:57 +0200

    Add production api url

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:3000';
 export const IS_DEVELOP = process.env.NODE_ENV !== 'production';
+export const API_URL = IS_DEVELOP ? 'http://localhost:3000' : 'https://liquid-api.herokuapp.com/';

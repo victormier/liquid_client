526acfd9d9dde8a8ebf12d131f1bcb743f56500a
victormier <victormier@gmail.com>
2017-09-13 17:28:40 +0200

    Return index for any route

--- src/server.js
+++ src/server.js
@@ -2,27 +2,27 @@ const path = require('path')
 const express = require('express')
 
 const getApp = function () {
-  const app = express()
-  const indexPath = path.join(__dirname, '../dist/index.html')
-  const publicPath = express.static(path.join(__dirname, '../dist'))
+  const app = express();
+  const indexPath = path.join(__dirname, '../dist/index.html');
+  const publicPath = express.static(path.join(__dirname, '../dist'));
 
-  app.use('/', publicPath)
-  app.get('/', function (_, res) { res.sendFile(indexPath) })
+  app.use('/', publicPath);
+  app.get('*', function (_, res) { res.sendFile(indexPath) });
 
-  return app
+  return app;
 }
 
-const port = (process.env.PORT || 8080)
-const app = getApp()
+const port = (process.env.PORT || 8080);
+const app = getApp();
 
 if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('../webpack.dev.config.js')
-  const compiler = webpack(config)
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const config = require('../webpack.dev.config.js');
+  const compiler = webpack(config);
 
-  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackHotMiddleware(compiler));
   app.use(webpackDevMiddleware(compiler, {
     noInfo: true,
     publicPath: config.output.publicPath

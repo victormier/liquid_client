8e36075f14be4561ba822445baae5f43d6105c44
victormier <victormier@gmail.com>
2017-09-18 17:30:17 +0200

    Redirect to accounts on public routes

--- src/components/common/PublicRoutes/index.jsx
+++ src/components/common/PublicRoutes/index.jsx
@@ -1,18 +1,15 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { inject } from 'mobx-react';
-import { withApollo } from 'react-apollo';
 
-@inject('sessionStore')
 class PublicRoutes extends Component {
   componentWillMount() {
-    if (this.props.sessionStore.authenticated) {
-      this.props.router.push('/settings');
+    if (this.props.route.authenticated) {
+      this.props.router.push('/accounts');
     }
   }
 
   render() {
-    if (!this.props.sessionStore.authenticated) {
+    if (!this.props.route.authenticated) {
       return this.props.children;
     }
     return null;
@@ -23,16 +20,13 @@ PublicRoutes.propTypes = {
   router: PropTypes.shape({
     push: PropTypes.func.isRequired,
   }).isRequired,
+  route: PropTypes.shape({
+    authenticated: PropTypes.bool.isRequired,
+  }),
   children: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]).isRequired,
 };
 
-PublicRoutes.wrappedComponent.propTypes = {
-  sessionStore: PropTypes.shape({
-    authenticated: PropTypes.bool.isRequired,
-  }).isRequired,
-};
-
-export default withApollo(PublicRoutes);
+export default PublicRoutes;

12fe3f66fad170e976c20b4b85a03925fb708819
victormier <victormier@gmail.com>
2017-11-03 18:54:51 +0100

    Add production url

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,10 @@ export const STAGING_CLIENT_HOST = 'liquid-client-staging.herokuapp.com';
 export const PRODUCTION_CLIENT_HOST_HEROKU = 'liquid-client.herokuapp.com';
 export const PRODUCTION_CLIENT_HOST = 'app.helloliquid.com';
 
+export const API_HOST_DEVELOPMENT = 'localhost:3000';
+export const API_HOST_STAGING = 'liquid-api-staging.herokuapp.com';
+export const API_HOST_PRODUCTION = 'api.helloliquid.com';
+
 let liquidEnvironment;
 if (process.env.NODE_ENV === 'production') {
   if ((window.location.hostname.toLowerCase().search(PRODUCTION_CLIENT_HOST) < 0) ||
@@ -19,16 +23,16 @@ let apiHost;
 let mixpanelToken;
 switch (liquidEnvironment) {
   case 'development':
-    apiHost = DEVELOPMENT_CLIENT_HOST;
+    apiHost = `http://${API_HOST_DEVELOPMENT}`;
     mixpanelToken = '8031cf315d6b0b92f22892fe2ee70685';
     break;
   case 'staging':
-    apiHost = STAGING_CLIENT_HOST;
+    apiHost = `https://${API_HOST_STAGING}`;
     mixpanelToken = 'a7c4b21b8d0d7499f56ba532eaeb5e4c';
     break;
   default:
-    apiHost = PRODUCTION_CLIENT_HOST;
+    apiHost = `https://${API_HOST_PRODUCTION}`;
     mixpanelToken = 'dc1a320436debda6a29ea4e58c5fb631';
 }
-export const API_URL = `https://${apiHost}`;
+export const API_URL = apiHost;
 export const MIXPANEL_TOKEN = mixpanelToken;

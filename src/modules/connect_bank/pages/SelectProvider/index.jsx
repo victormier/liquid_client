d12e97eab68233ced71f50bc8144eece77eeeed5
victormier <victormier@gmail.com>
2017-11-08 07:51:52 +0100

    Add event for bank select

--- src/modules/connect_bank/pages/SelectProvider/index.jsx
+++ src/modules/connect_bank/pages/SelectProvider/index.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import { connect } from 'react-redux';
 import { reduxForm, Field, formValueSelector } from 'redux-form';
 import PropTypes from 'prop-types';
 import FormInput from 'components/common/FormInput';
 import gridStyles from 'styles/base/grid.scss';
+import { inject } from 'mobx-react';
+import { autobind } from 'core-decorators';
+import { mixpanelEventProps, SELECT_BANK } from 'config/mixpanelEvents';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import { queryAllSaltedgeProviders } from 'qql';
 import _ from 'lodash';
@@ -12,17 +15,46 @@ import { getCountryName } from 'utils/iso-countries';
 import { Link } from 'react-router';
 import styles from './styles.scss';
 
-const BankItem = props => (
-  <li className={styles.bankItem}>
-    <Link to={`/connect/providers/${props.saltedgeProvider.id}`}>
-      { props.saltedgeProvider.name }
-    </Link>
-  </li>
-);
+@inject('mixpanel')
+class BankItem extends Component {
+  @autobind
+  trackEvent() {
+    const { saltedgeProvider } = this.props;
+    const bankIdentifier = `${saltedgeProvider.name} [${saltedgeProvider.country_code}] [${saltedgeProvider.id}]`;
+    const eventProps = {
+      ...mixpanelEventProps(SELECT_BANK),
+      'Bank country': saltedgeProvider.country_code,
+      'Bank name': saltedgeProvider.name,
+    };
+    this.props.mixpanel.track(SELECT_BANK, { 'Bank selected': bankIdentifier });
+    this.props.mixpanel.register(eventProps);
+    this.props.mixpanel.people.set(eventProps);
+  }
+
+  render() {
+    return (
+      <li className={styles.bankItem}>
+        <Link to={`/connect/providers/${this.props.saltedgeProvider.id}`} onClick={this.trackEvent}>
+          { this.props.saltedgeProvider.name }
+        </Link>
+      </li>
+    );
+  }
+}
 BankItem.propTypes = {
   saltedgeProvider: PropTypes.shape({
     name: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
+  }).isRequired,
+};
+BankItem.wrappedComponent.propTypes = {
+  mixpanel: PropTypes.shape({
+    track: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
+    identify: PropTypes.func.isRequired,
+    people: PropTypes.shape({
+      set: PropTypes.func.isRequired,
+    }),
   }).isRequired,
 };
 

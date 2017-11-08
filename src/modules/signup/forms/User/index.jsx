fa4d64350b51dbe3e8e1246be08069d245968541
victormier <victormier@gmail.com>
2017-11-08 12:11:22 +0100

    Fix merging error

--- src/modules/signup/forms/User/index.jsx
+++ src/modules/signup/forms/User/index.jsx
@@ -1,49 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { Row, Col } from 'react-flexbox-grid';
 import Button from 'components/common/Button';
 import FormInput from 'components/common/FormInput';
 import PropTypes from 'prop-types';
-import { autobind } from 'core-decorators';
-import { inject } from 'mobx-react';
-import { INPUT_SIGNUP_EMAIL } from 'config/mixpanelEvents';
 import styles from './styles.scss';
 
-@inject('mixpanel')
-class EmailInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { changeFired: false };
-  }
-
-  @autobind
-  handleChange(evt) {
-    if (!this.state.changeFired) {
-      this.setState({ changeFired: true }, () => {
-        this.props.mixpanel.track(INPUT_SIGNUP_EMAIL);
-      });
-    }
-    this.props.input.onChange(evt);
-  }
-
-  render() {
-    return (<FormInput
-      value={this.props.input.value}
-      onChange={this.handleChange}
-      type="text"
-      placeholder="Email"
-    />);
-  }
-}
-EmailInput.wrappedComponent.propTypes = {
-  mixpanel: PropTypes.shape({
-    track: PropTypes.func.isRequired,
-  }).isRequired,
-  input: PropTypes.shape({
-    onChange: PropTypes.func.isRequired,
-    value: PropTypes.string,
-  }).isRequired,
-};
+const EmailInput = componentProps => (
+  <FormInput
+    value={componentProps.input.value}
+    onChange={componentProps.input.onChange}
+    type="text"
+    placeholder="Email"
+  />);
 
 const UserForm = (props) => {
   const { handleSubmit, submitting } = props;

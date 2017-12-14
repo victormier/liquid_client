55ffa8ba1413757321d9962870de7b23544ea823
victormier <victormier@gmail.com>
2017-12-14 14:20:44 +0100

    Better header styles

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -2,37 +2,46 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 import baseStyles from 'styles/base/base.scss';
+import gridStyles from 'styles/base/grid.scss';
 import GoBackArrow from 'components/common/GoBackArrow';
 import styles from './styles.scss';
 
 const Header = props => (
-  <Row className={baseStyles.topNav}>
-    <Col xs={6}>
-      {
-        props.backTo ?
-          <GoBackArrow to="/accounts" /> :
-          props.leftButton
-      }
-    </Col>
-    <Col xs={2}>
-      <div className={baseStyles.floatRight}>
-        {props.rightButtonSecondary}
-      </div>
-    </Col>
-    <Col xs={4} className={baseStyles.textRight}>
-      {props.rightButton}
-    </Col>
-    <Col xs={6}>
-      <h1 className={styles.title}>{props.title}</h1>
-      <div className={styles.subtitle}>{props.subtitle}</div>
-    </Col>
-    <Col xs={6}>
-      <h2 className={baseStyles.textRight}>
-        { props.titleRight }
-      </h2>
-    </Col>
-    <Col xs={12}><hr /></Col>
-  </Row>
+  <div>
+    <Row className={styles.topRow}>
+      <Col xs={6}>
+        <div className={gridStyles.gridDisplacedLeft}>
+          {
+            props.backTo ?
+              <GoBackArrow to="/accounts" /> :
+              props.leftButton
+          }
+        </div>
+      </Col>
+      <Col xs={2}>
+        <div className={baseStyles.floatRight}>
+          {props.rightButtonSecondary}
+        </div>
+      </Col>
+      <Col xs={4} className={baseStyles.textRight}>
+        <div className={gridStyles.gridDisplacedRight}>
+          {props.rightButton}
+        </div>
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={6}>
+        <h1 className={styles.title}>{props.title}</h1>
+        <div className={styles.subtitle}>{props.subtitle}</div>
+      </Col>
+      <Col xs={6}>
+        <h2 className={baseStyles.textRight}>
+          { props.titleRight }
+        </h2>
+      </Col>
+      <Col xs={12}><hr /></Col>
+    </Row>
+  </div>
 );
 
 Header.propTypes = {

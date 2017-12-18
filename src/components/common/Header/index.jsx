b553677e8cba8baf821a369cc1835cd5523b809d
victormier <victormier@gmail.com>
2017-12-18 11:01:53 +0100

    Add mini header option

--- src/components/common/Header/index.jsx
+++ src/components/common/Header/index.jsx
@@ -6,43 +6,43 @@ import gridStyles from 'styles/base/grid.scss';
 import GoBackArrow from 'components/common/GoBackArrow';
 import styles from './styles.scss';
 
-const Header = props => (
-  <div>
-    <Row className={styles.topRow}>
-      <Col xs={6}>
-        <div className={gridStyles.gridDisplacedLeft}>
-          {
+const Header = props => (<div>
+  <Row className={styles.topRow}>
+    <Col xs={6}>
+      <div className={gridStyles.gridDisplacedLeft}>
+        {
             props.backTo ?
               <GoBackArrow to={props.backTo} /> :
               props.leftButton
           }
-        </div>
-      </Col>
-      <Col xs={2}>
-        <div className={baseStyles.floatRight}>
-          {props.rightButtonSecondary}
-        </div>
-      </Col>
-      <Col xs={4} className={baseStyles.textRight}>
-        <div className={gridStyles.gridDisplacedRight}>
-          {props.rightButton}
-        </div>
-      </Col>
-    </Row>
-    <Row>
-      <Col xs={8}>
-        <h1 className={styles.title}>{props.title}</h1>
-        <div className={styles.subtitle}>{props.subtitle}</div>
-      </Col>
-      <Col xs={4}>
-        <h2 className={styles.titleRight}>
-          { props.titleRight }
-        </h2>
-      </Col>
-      <Col xs={12}><hr /></Col>
-    </Row>
-  </div>
-);
+      </div>
+    </Col>
+    <Col xs={2}>
+      <div className={baseStyles.floatRight}>
+        {props.rightButtonSecondary}
+      </div>
+    </Col>
+    <Col xs={4} className={baseStyles.textRight}>
+      <div className={gridStyles.gridDisplacedRight}>
+        {props.rightButton}
+      </div>
+    </Col>
+  </Row>
+  { !props.mini &&
+  <Row>
+    <Col xs={8}>
+      <h1 className={styles.title}>{props.title}</h1>
+      <div className={styles.subtitle}>{props.subtitle}</div>
+    </Col>
+    <Col xs={4}>
+      <h2 className={styles.titleRight}>
+        { props.titleRight }
+      </h2>
+    </Col>
+    <Col xs={12}><hr /></Col>
+  </Row>
+  }
+</div>);
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
@@ -52,6 +52,7 @@ Header.propTypes = {
   rightButton: PropTypes.object,
   rightButtonSecondary: PropTypes.object,
   backTo: PropTypes.string,
+  mini: PropTypes.boolean,
 };
 
 export default Header;

448c6956fea156fe227f4497f418a162501776ba
victormier <victormier@gmail.com>
2017-12-20 14:13:24 +0100

    Displace account blocks

--- src/modules/main/components/Account/index.jsx
+++ src/modules/main/components/Account/index.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
-import Button from 'components/common/Button';
 import { toCurrency } from 'utils/currencies';
+import Button from 'components/common/Button';
 import styles from './styles.scss';
 
 const Account = props => (
-  <Button
-    color="lightBlueGradient"
-    className={styles.account}
-    shape="squareRounded"
-    {..._.omit(props, ['account'])}
-  >
-    <div className={`${styles.accountInfo} ${styles.accountDataBlock}`}>
-      <div className={styles.accountDataContent}>
-        <div className={styles.accountName}>{props.account.name}</div>
-        { props.account.is_mirror_account &&
-        <div className={styles.accountBank}>{props.account.provider_name}</div>
-        }
+  <div className={styles.accountContainer}>
+    <Button
+      color="lightBlueGradient"
+      className={styles.account}
+      shape="squareRounded"
+      {..._.omit(props, ['account'])}
+    >
+      <div className={`${styles.accountInfo} ${styles.accountDataBlock}`}>
+        <div className={styles.accountDataContent}>
+          <div className={styles.accountName}>{props.account.name}</div>
+          { props.account.is_mirror_account &&
+          <div className={styles.accountBank}>{props.account.provider_name}</div>
+          }
+        </div>
       </div>
-    </div>
-    <div className={`${styles.accountBalance} ${styles.accountDataBlock}`}>
-      <div className={styles.accountDataContent}>
-        {toCurrency(props.account.balance, props.account.currency_code)}
+      <div className={`${styles.accountBalance} ${styles.accountDataBlock}`}>
+        <div className={styles.accountDataContent}>
+          {toCurrency(props.account.balance, props.account.currency_code)}
+        </div>
       </div>
-    </div>
-  </Button>
+    </Button>
+  </div>
 );
 
 Account.propTypes = {

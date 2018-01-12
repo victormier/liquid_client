75ee96e7e34eb66057d9b2a7c96083d6c51b3870
victormier <victormier@gmail.com>
2018-01-12 11:54:38 +0100

    Add styles for inputs

--- src/components/common/FormInput/index.jsx
+++ src/components/common/FormInput/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
 import styles from './styles.scss';
 
-const FormInput = props => (
-  <input
-    {...props}
-    className={styles.input}
-  />
-);
+const FormInput = (props) => {
+  const formId = props.label ? _.camelCase(props.label) : _.uniqueId('input');
+
+  return (<div className={styles.inputBlock}>
+    <div className={styles.inputContainer}>
+      <input
+        id={formId}
+        {...props}
+        className={styles.input}
+      />
+    </div>
+    { props.label &&
+    <label htmlFor={formId}>{props.label}</label> }
+  </div>);
+};
+
+FormInput.propTypes = {
+  label: PropTypes.string,
+};
 
 export default FormInput;
